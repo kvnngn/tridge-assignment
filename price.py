7a6a7d4a8689c385d812ed4ed9848e7e04014b82
knguyen <kevin3.nguyen@epitech.eu>
2020-11-25 15:59:16 +0900

    WIP: write to csv

--- price.py
+++ price.py
@@ -2,20 +2,18 @@ import csv
 import moment
 from datetime import datetime
 import pandas as pd
+import json
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.strptime(d2, "%Y-%m-%d")
     return abs((d2 - d1).days)
 
-def getWeeklyTrends(currentWeek, nextWeek):
-	if (days_between(column[0], previousColumn[0]) == 7):
-		currentWeekPrice = float(currentWeek[1])
-		nextWeekPrice = float(nextWeek[1])
-		increase = nextWeekPrice - currentWeekPrice
-		getWeeklyTrends.weeklyTrends[nextWeek[0]] = str(round((increase / nextWeekPrice) * 100, 2)) + '%'
-	else:
-		getWeeklyTrends.weeklyTrends[nextWeek[0]] = ''
+def calculateTrendPrice(currentWeek, nextWeek):
+	currentWeekPrice = float(currentWeek[1])
+	nextWeekPrice = float(nextWeek[1])
+	increase = nextWeekPrice - currentWeekPrice
+	calculateTrendPrice.weeklyTrends[nextWeek[0]] = str(round((increase / nextWeekPrice) * 100, 2)) + '%'
 
 def getMonthlyPrice(currentWeek):
 	date = datetime.strptime(currentWeek[0], "%Y-%m-%d")
@@ -32,18 +30,26 @@ with open('dataset.csv') as f:
         for columnIndex, column in enumerate(row.items()):
         	if (columnIndex == 0):
         		getMonthlyPrice.averagePricePerMonth = {}
-        		getWeeklyTrends.weeklyTrends = {}
+        		calculateTrendPrice.weeklyTrends = {}
         	if (columnIndex == 0 and rowIndex == 0):
         		df_marks = pd.DataFrame(columns=row.keys())
         	if (columnIndex > 4):
         		if (len(column[1]) > 1 and previousColumn is not None):
-        			getWeeklyTrends(column, previousColumn)
+        			if (days_between(column[0], previousColumn[0]) == 7):
+        				calculateTrendPrice(column, previousColumn)
+        			else:
+        				calculateTrendPrice.weeklyTrends[previousColumn[0]] = ''
         		if (len(column[1]) > 1): # TODO: check if it's a price instead of columnIndex
         			previousColumn = column[:]
         			getMonthlyPrice(column)
-        print('Row monthly',rowIndex + 3,'processed', getMonthlyPrice.averagePricePerMonth)
-        print('Row weeklyTrends',rowIndex + 3,getWeeklyTrends.weeklyTrends)
-        new_row = getWeeklyTrends.weeklyTrends.update(getMonthlyPrice.averagePricePerMonth)
+        	else:
+        		calculateTrendPrice.weeklyTrends[column[0]] = column[1]
+        if (len(getMonthlyPrice.averagePricePerMonth) > 1):
+        	for item in getMonthlyPrice.averagePricePerMonth:
+        		print(item)
+        print('Monthly average price',rowIndex + 3,'processed', getMonthlyPrice.averagePricePerMonth)
+        print('Weekly trends price',rowIndex + 3,calculateTrendPrice.weeklyTrends)
+        new_row = {**calculateTrendPrice.weeklyTrends, **getMonthlyPrice.averagePricePerMonth}
         print('NEW ROWWW',new_row)
         df_marks = df_marks.append(new_row, ignore_index=True)
     print(df_marks)
